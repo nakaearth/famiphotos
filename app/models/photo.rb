5c5a0bf16b6e6cc6df7fe13089214e3b2a00a710
nakamura shinichirou <naka5313@gmail.com>
2015-05-17 00:15:09 +0900

    photoクラスのモデルテスト追加

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,2 +1,5 @@
 class Photo < ActiveRecord::Base
+  belongs_to :user, dependent: :destroy
+
+  validates :description, length: { maximum: 140 }
 end
--- spec/models/photo_spec.rb
+++ spec/models/photo_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Photo, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { create(:user) }
+  let(:photo) { create(:photo, user: user) }
+
+  describe 'association' do
+    context 'have a relation to photo class' do
+      it { expect belong_to(:user) }
+    end
+  end
+
+  describe 'validation' do
+    context 'description column' do
+      it { expect ensure_length_of(:descripton).is_at_most(140) }
+    end
+  end
 end
