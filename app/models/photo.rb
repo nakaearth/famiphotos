9afccbbca7cc87497f209181deea97a64b735ab7
nakamura shinichirou <Nakamura.Shinichiro@moneyforward.co.jp>
2015-05-18 20:51:47 +0900

    追加

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,6 +1,8 @@
 class Photo < ActiveRecord::Base
   include IdEncryptable
 
+  mount_uploader :photo, PhotoUploader
+
   belongs_to :user, dependent: :destroy
 
   validates :description, length: { maximum: 140 }
