5b742054123257b653fe7db6839a5cfbc8c1b127
nakamura shinichirou <naka5313@gmail.com>
2015-05-20 01:53:12 +0900

    rubocop対応

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,7 +1,7 @@
 class TopController < ApplicationController
   skip_before_action :login?
 
-  # トップ画面 
+  # トップ画面
   def index
   end
 end
--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -11,17 +11,17 @@ class PhotoUploader < CarrierWave::Uploader::Base
   storage :file
   # storage :fog
 
-  process :convert => 'png'
-  process :tags => ['post_picture']
+  process convert: 'png'
+  process tags: ['post_picture']
 
   version :standard do
-    process :resize_to_fill => [100, 150, :north]
+    process resize_to_fill: [100, 150, :north]
   end
-  
+
   version :thumbnail do
     resize_to_fit(50, 50)
   end
- 
+
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
@@ -59,5 +59,4 @@ class PhotoUploader < CarrierWave::Uploader::Base
   # def filename
   #   "something.jpg" if original_filename
   # end
-
 end
