10958bddf6e5d16e6fa159e3ad7620df212c2b80
nakamura shinichirou <Nakamura.Shinichiro@moneyforward.co.jp>
2015-06-30 10:22:22 +0900

    test 追記

--- spec/controllers/photos_controller_spec.rb
+++ spec/controllers/photos_controller_spec.rb
@@ -12,10 +12,16 @@ RSpec.describe PhotosController, type: :controller do
   end
 
   describe "GET #index" do
-    it "returns http success" do
+    before do
       get :index
+    end
+
+    it "returns http success" do
       expect(response).to have_http_status(:success)
     end
+
+    it { expect(assigns[:photos]).not_to be_nil }
+    it { expect(assigns[:photos].size).to eq 1 }
   end
 
   describe "GET #show" do
@@ -33,6 +39,10 @@ RSpec.describe PhotosController, type: :controller do
   end
 
   describe "GET #create" do
+    before do
+      Paperclip::Attachment.any_instance.stubs(:save).returns(true)
+    end
+
     it "returns http success" do
       get :create
       expect(response).to have_http_status(:success)
