06f9cc964e0e34428caa44fc88147ff712d665a8
nakamura shinichirou <Nakamura.Shinichiro@moneyforward.co.jp>
2015-07-06 09:40:17 +0900

    追加です

--- spec/controllers/api/informations_controller_spec.rb
+++ spec/controllers/api/informations_controller_spec.rb
@@ -3,13 +3,17 @@ require 'rails_helper'
 RSpec.describe Api::InformationsController, type: :controller do
   render_views
 
-  let(:information) { create(:information) }
+  let(:information) { create_list(:information, 5) }
 
   describe "GET #index" do
-    it "returns http success" do
+    before do
       get :index
+    end
+
+    it "returns http success" do
       expect(response).to have_http_status(:success)
     end
 
+    it { expect(assigns[:informations].size).to eq 5 }
   end
 end
