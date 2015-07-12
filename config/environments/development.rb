000f44a29ec918b75d39904516e6e2999cd8af14
nakamura shinichirou <naka5313@gmail.com>
2015-07-13 00:21:35 +0900

    追加

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,8 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # react js
+  config.react.variant = :development
+  config.react.addons = true
 end
