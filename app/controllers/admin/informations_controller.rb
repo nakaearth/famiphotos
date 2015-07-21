49246849617699310ee06a7a63e06ed231041f1e
nakamura shinichirou <naka5313@gmail.com>
2015-07-21 23:10:18 +0900

    admin追加

--- app/controllers/admin/informations_controller.rb
+++ app/controllers/admin/informations_controller.rb
@@ -1,4 +1,31 @@
 module Admin
   class InformationsController < Admin::ApplicationController
+    before_action :set_information, only: [:show, :destroy]
+
+    def index
+      @informations = Information.all
+    end
+
+    def show
+    end
+
+    def new
+      @information = Information.new
+    end
+
+    def create
+    end
+
+    def destroy
+      @information.destroy
+
+      redirect_to admin_informations_path, notice: 'お知らせを削除しました'
+    end
+
+    private
+
+    def set_information
+      @information = Information.find(params[:id])
+    end
   end
 end
