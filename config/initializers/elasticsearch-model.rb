63727dbc454ae76e42ffb3004b9ddd7ef527d2ca
nakaearth <naka5313@gmail.com>
2015-08-18 09:44:23 +0900

    設定を一部変更

--- config/initializers/elasticsearch-model.rb
+++ config/initializers/elasticsearch-model.rb
@@ -1,5 +1,5 @@
 Elasticsearch::Model.client = Elasticsearch::Client.new(
-  host: "localhost", port: 9200,  
+  host: "192.168.25.20", port: 9200,  
   retry_on_failure: true, 
   reload_connection: true, 
   randomize_hosts: true, 
