79f40049f9fffc323fade7288be27cd65b62b007
nakaearth <naka5313@gmail.com>
2015-08-18 18:35:51 +0900

    複数サーバ対応

--- config/initializers/elasticsearch-model.rb
+++ config/initializers/elasticsearch-model.rb
@@ -1,5 +1,5 @@
 Elasticsearch::Model.client = Elasticsearch::Client.new(
-  host: "192.168.25.20", port: 9200,  
+  hosts: [ { host: "192.168.25.20", port: 9200 }, { host: "192.168.25.30", port: 9200 } ],  
   retry_on_failure: true, 
   reload_connection: true, 
   randomize_hosts: true, 
