a892bcb8388e0683e9cb7be6260ae3cc08e16e49
nakamura shinichirou <naka5313@gmail.com>
2015-08-20 00:28:55 +0900

    scorを追加

--- app/services/search/photo_service.rb
+++ app/services/search/photo_service.rb
@@ -8,6 +8,7 @@ module Search
     #
     def search(photo_search, user)
       @client.search(
+        min_score:  0.5, 
         query: {
           match: { "description": "#{photo_search.search_word}" }
         },
