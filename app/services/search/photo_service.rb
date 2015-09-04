5baf96fb50ad28e1cad8f899af7266d7be846dce
nakamura shinichirou <naka5313@gmail.com>
2015-09-05 02:52:47 +0900

    設定和まりサイドみなおし

--- app/services/search/photo_service.rb
+++ app/services/search/photo_service.rb
@@ -1,15 +1,13 @@
 module Search
   class PhotoService
     def initialize(search_model_class)
-      if search_model_class.columns.any? { |column| column.type == 'text' }
+      if search_model_class.columns { |column| column.type == 'text' }.any?
         @client = search_model_class.__elasticsearch__
       else
-        fail "Format error: #{search_model_class}do not have text type column"
+        fail "Format error: #{search_model_class} do not have text type column"
       end
     end
 
-    # TODO: 柔軟生を出すようんいする.titleがカラムにあるかみるようにsuru
-    #
     def search(photo_search, user)
       @client.search(
         min_score:  0.3,
