251078f04b6692dd6def8a98f9233a7d3d8cb081
nakamura shinichirou <naka5313@gmail.com>
2015-09-27 08:04:47 +0900

    controlerをtuika

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,48 @@
+module Admin
+  class UsersController < Admin::ApplicationController
+    before_action :set_user, only: [:show, :edit, :destroy]
+
+    def index
+      @users = User.all
+    end
+
+    def show
+    end
+
+    def new
+      @user = User.new
+    end
+
+    def create
+      @user = User.new(create_params)
+
+      if @user.save
+        redirect_to :admin_users_path, notice: '新たにユーザを追加しました'
+      else
+        render :new
+      end
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def destroy
+      @user.destroy
+
+      redirect_to admin_users_path, notice: 'お知らせを削除しました'
+    end
+
+    private
+
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    def crete_params
+      params.require(:user).permit(:name, :email)
+    end
+  end
+end
