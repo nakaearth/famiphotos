3b1a60250b6a1080d5043d8bcac9baf8fac306eb
nakamura shinichirou <naka5313@gmail.com>
2015-10-02 01:14:52 +0900

    追加

---
+++ app/controllers/group_members_controller.rb
@@ -0,0 +1,29 @@
+class GroupMembersController < ApplicationController
+
+  def index
+    @users = current_group.users
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def group_member_params
+  end
+end
