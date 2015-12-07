8516696eb297f3e88e69024e733218e2ac277232
nakaearth <naka5313@gmail.com>
2015-12-07 14:03:59 +0900

    変更一部

--- config/initializers/switch_point.rb
+++ config/initializers/switch_point.rb
@@ -1,13 +1,13 @@
 SwitchPoint.configure do |config|
-  if Rails.env.test?
-    config.define_switch_point :famiphoto,
-      readonly: :"famiphotos_slave_#{Rails.env}",
-      writable: :"famiphotos_master_#{Rails.env}"
-  end
-
-#  unless Rails.env.test?
+#  if Rails.env.test?
 #    config.define_switch_point :famiphoto,
 #      readonly: :"famiphotos_slave_#{Rails.env}",
 #      writable: :"famiphotos_master_#{Rails.env}"
 #  end
+
+  unless Rails.env.test?
+    config.define_switch_point :famiphoto,
+      readonly: :"famiphotos_slave_#{Rails.env}",
+      writable: :"famiphotos_master_#{Rails.env}"
+  end
 end
