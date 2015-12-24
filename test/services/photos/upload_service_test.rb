4b91692bdc3c93c81f6ec18c0b1976e2d2b9fae2
nakamura shinichirou <naka5313@gmail.com>
2015-12-25 02:21:31 +0900

    テスト周りを修正

--- test/services/photos/upload_service_test.rb
+++ test/services/photos/upload_service_test.rb
@@ -1,12 +1,17 @@
 require "test_helper"
 
 class Photos::UploadServiceTest < ActiveSupport::TestCase
-  def setup
-  end
-
-  def teardown
+  setup do
+    @photo_params = {
+      description: 'これはテスト', 
+      image: "#{Rails.root}/test/fixtures/test.jpg", 
+      photo_geo_attributes: [
+        address: '東京都港区'
+      ]
+    }
   end
 
   def test_file_upload
+    assert_nothing_raised { Photos::UploadService.execute(create(:user), @photo_params) }
   end
 end
