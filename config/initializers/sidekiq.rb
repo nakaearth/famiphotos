3135f111ef7d455af44be36e6910c15ef8e756aa
nakaearth <naka5313@gmail.com>
2016-01-21 18:44:36 +0900

    redisの設定をプラス

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,3 +1,16 @@
+redis_config = Rails.application.config_for(:redis)
+
+redis_connection = proc {
+  Redis.new(
+    host: redis_config[:host],
+    port: redis_config[:port],
+    namespace: "#{Rails.env}:#{redis_config['namespace']}",
+    password: redis_config['password']
+  )
+}
+
+
+# TODO: ここ修正する
 Sidekiq.configure_server do |config|
   case Rails.env
     when 'production' then
@@ -5,6 +18,7 @@ Sidekiq.configure_server do |config|
     else
       # config.redis = { url: 'redis://192.168.25.30:6379', namespace: 'sidekiq' }
       config.redis = { url: 'redis://127.0.0.1:6379', namespace: 'sidekiq' }
+      config.redis = ConnectionPool.new(size: 5,  &redis_connection)
   end
 end
 
