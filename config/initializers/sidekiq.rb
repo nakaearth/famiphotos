6df168e1fade3e06b3068a1df9d7eb51b3c47522
nakamura shinichirou <naka5313@gmail.com>
2016-01-22 00:06:58 +0900

    reidsの設定追加

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -9,25 +9,10 @@ redis_connection = proc {
   )
 }
 
-
-# TODO: ここ修正する
 Sidekiq.configure_server do |config|
-  case Rails.env
-    when 'production' then
-      config.redis = { url: 'redis://production.redis-hoge.com:6379', namespace: 'sidekiq' }
-    else
-      # config.redis = { url: 'redis://192.168.25.30:6379', namespace: 'sidekiq' }
-      config.redis = { url: 'redis://127.0.0.1:6379', namespace: 'sidekiq' }
-      config.redis = ConnectionPool.new(size: 5,  &redis_connection)
-  end
+  config.redis = ConnectionPool.new(size: 5,  &redis_connection)
 end
 
 Sidekiq.configure_client do |config|
-  case Rails.env
-    when 'production' then
-      config.redis = { url: 'redis://production.redis-hoge.com:6379', namespace: 'sidekiq' }
-    else
-      # config.redis = { url: 'redis://192.168.25.30:6379', namespace: 'sidekiq' }
-      config.redis = { url: 'redis://127.0.0.1:6379', namespace: 'sidekiq' }
-  end
+  config.redis = ConnectionPool.new(size: 5,  &redis_connection)
 end
