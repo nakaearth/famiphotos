fde41ee7414bc05d5232b6d06913260e7fe305be
nakamura shinichirou <naka5313@gmail.com>
2016-04-15 23:55:55 +0900

    暗号化ロジック追加

--- app/models/concerns/encryptedable.rb
+++ app/models/concerns/encryptedable.rb
@@ -1,12 +1,26 @@
 module Encryptedable
   extend ActiveSupport::Concern
 
+
+  included do
+    before_save :encrypted
+
+    class_attribute :encrypt_columns
+    @encrypt_columns = []
+  end
+
   module ClassMethods
     def attr_encrypted(*columns)
       columns.map(&:to_s).each do |column|
-        value = Base64.encode64(read_attribute(column))
-        write_attribute(column, value) 
+        @encrypt_columns << column
       end 
+    end
+  end
+
+  def encrypted
+    @encrypted_columns.each do |encrypted_column|
+      value = Base64.encode64(read_attribute(column))
+      write_attribute(column, value) 
     end
   end
 end
