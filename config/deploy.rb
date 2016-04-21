b2e08c8e4b497aee02aef82fd4597e234bdb9859
nakamura shinichirou <naka5313@gmail.com>
2016-04-21 22:34:04 +0900

    設定を変更

--- config/deploy.rb
+++ config/deploy.rb
@@ -49,7 +49,9 @@ set :whenever_identifier, ->{ "#{fetch(:application)}_#{fetch(:stage)}" }
 
 namespace :app do
   task :update_rvm_key do
-    execute :gpg, "--keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3"
+    on roles(:app) do
+      execute "gpg --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3"
+    end
   end
 end
 before "rvm1:install:rvm", "app:update_rvm_key"
