74e592c066ac964129622e5f3c0a0d07a8f9d2fa
nakamura shinichirou <naka5313@gmail.com>
2016-04-24 23:45:15 +0900

    テストを追加

--- test/models/album_test.rb
+++ test/models/album_test.rb
@@ -5,7 +5,24 @@ class AlbumTest < ActiveSupport::TestCase
     @album ||= Album.new
   end
 
+  def test_title_is_nil
+    group = create(:group)
+    album = Album.new(group: group, title: nil)
+
+    value(album).must_be :invalid?
+  end
+  
+  def test_title_length_is_over_80
+    group = create(:group)
+    album = Album.new(group: group, title: ('abcdefghij' * 8).concat('1'))
+
+    value(album).must_be :invalid?
+  end
+  
   def test_valid
-    assert album.valid?
+    group = create(:group)
+    album = Album.new(group: group, title: 'abcdefghij' * 8)
+
+    value(album).must_be :valid?
   end
 end
