c8358928b40425dcc01ea5a658b594c366bd8d6b
nakamura shinichirou <naka5313@gmail.com>
2016-05-03 08:00:18 +0900

    日本語ファイル追加

--- app/models/group_member.rb
+++ app/models/group_member.rb
@@ -1,5 +1,9 @@
 class GroupMember < FamiphotoBase
+  extend Enumerize
+
   belongs_to :user
   belongs_to :group
   scope :not_group_member, ->(user_ids) { where.not('user: ?', user_ids) }
+
+  enumerize :role, [:owner, :member]
 end
