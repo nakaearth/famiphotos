b4f53add45fdfad5a631d788f982d6891f5526c2
nakamura shinichirou <naka5313@gmail.com>
2016-05-04 07:54:47 +0900

    newのviewを追加

--- app/models/group.rb
+++ app/models/group.rb
@@ -4,7 +4,15 @@ class Group < FamiphotoBase
   has_many :users, through: :group_members, source: :user
   has_many :albums, dependent: :destroy
 
+  accepts_nested_attributes_for :group_members
   accepts_nested_attributes_for :albums
 
   validates :name, presence: true
+
+  # group作成後group_memberも作成する
+  def save_with_group_member(user, role)
+    return false unless save
+
+    GroupMember.new(user: user, group: self, role: role).save
+  end
 end
