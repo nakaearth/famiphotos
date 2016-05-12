86e788e92ee3d208ed50316f2ae24e497f0cd012
nakamura shinichirou <naka5313@gmail.com>
2016-05-13 07:59:02 +0900

    登校時にアルバムのIDを指定するように修正しないとだめ

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,12 +3,13 @@ class PhotosController < ApplicationController
   include DecryptedId
 
   before_action :set_request_variant
+  before_action :set_album, only: %i( index ) 
   before_action :set_photo, only: [:edit, :show, :destroy]
   before_action :set_photo_search
 
   def index
     respond_to do |format|
-      @photos = @current_user.photos.page(params[:page])
+      @photos = @album.photos.page(params[:page])
       format.html
 
       format.json { render 'api/photos/index' }
@@ -27,14 +28,18 @@ class PhotosController < ApplicationController
 
   def new
     @photo = @current_user.photos.build
+
+    @albums = @current_group.albums
   end
 
   def create
-    @photo = @current_user.photos.build(photo_params)
+    @photo = @album.photos.build(photo_params)
 
     if Photo.with_writable { @photo.save }
-      redirect_to photos_path, notice: '写真の登録がしました'
+      redirect_to album_photos_path(@album), notice: '写真の登録がしました'
     else
+      @albums = @current_group.albums
+
       render action: :new, alert: '写真の登録に失敗しました'
     end
   end
@@ -56,6 +61,10 @@ class PhotosController < ApplicationController
   def set_request_variant
     request.variant = :tablet if tablet?
     request.variant = :phone if mobile?
+  end
+
+  def set_album
+    @album = @current_group.albums.where(id: decrypted_id(params[:album_id])).first
   end
 
   def set_photo
