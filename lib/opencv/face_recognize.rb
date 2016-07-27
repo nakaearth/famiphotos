afc9b196c46f01cb459424831d5bb1b7e4ec6d62
nakamura shinichirou <naka5313@gmail.com>
2016-07-28 00:34:38 +0900

    rubocop対応

--- lib/opencv/face_recognize.rb
+++ lib/opencv/face_recognize.rb
@@ -1,20 +1,35 @@
 require 'opencv'
 include OpenCV
- 
+
 input_window = GUI::Window.new('Input')
 output_window = GUI::Window.new('Output')
- 
+
 begin
-  input_img = CvMat.load("public/images/dog2.jpg")
+  img = CvMat.load("public/images/dog2.jpg").resize(CvSize.new(200, 200))
 rescue
   puts '開けませんでした'
+
   exit
 end
- 
-output_img = input_img.BGR2GRAY.canny(120,200)
- 
-input_window.show input_img
-output_window.show output_img
- 
-GUI::wait_key
+
+# チャンネル分離
+b, g, r = img.split
+# [#<OpenCV::CvMat:>, #<OpenCV::CvMat:>, #<OpenCV::CvMat:>]
+
+zero = b.set_zero # NULLのかわり
+
+# 画像達
+images = [
+  img, # 元画像
+  (CvMat.merge b, zero, zero, zero), # 青
+  (CvMat.merge zero, g, zero, zero), # 緑
+  (CvMat.merge zero, zero, r, zero), # 赤
+  (CvMat.merge b, g, r), # 再合成
+  (CvMat.merge b, r, b) # 適当に組み換え
+].zip(%w(INPUT BLUE GREEN RED MERGED CHANGED)).each do|img, name|
+  GUI::Window.new(name).show img
+end
+
+# キーを押したら終了
+GUI.wait_key
 GUI::Window.destroy_all
