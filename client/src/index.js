f001af8eadc39e7a5959677e3c803df2ba3fbe09
nakaearth <naka5313@gmail.com>
2017-07-02 08:03:46 +0900

    reactの追加中

--- client/src/index.js
+++ client/src/index.js
@@ -4,16 +4,17 @@ import { render } from 'react-dom'
 class App extends Component {
   render() {
     const { title } = this.props
+    const { comment } = this.props
+    const { items } = ['test', 'hoge']
     return (
       <div>
-        <h1>
-          {title}
-        </h1>
+        <h1>{title}</h1>
+        <h1>{comment}</h1>
       </div>
     )
   }
 }
 
 render(
-  <App title="Hello React" />,
+  <App title="Hello React" comment="ほげホゲホゲ" />,
   document.getElementById('container'))
