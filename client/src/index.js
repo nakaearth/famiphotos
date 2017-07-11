41f6d47539445d4e8f8dc4c8f05dcfddaff86847
nakaearth <naka5313@gmail.com>
2017-07-12 00:13:40 +0900

    master merge

--- client/src/index.js
+++ client/src/index.js
@@ -1,26 +1,39 @@
 import React, { Component } from 'react'
 import { render } from 'react-dom'
 
+class AppList extends Component {
+  render() {
+		return (
+			<ul>
+				{this.props.items.map((item, index) => (
+					<li key={index}>{item}</li>
+				))}
+			</ul>
+		);
+  }
+}
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {items: ["hoge","hogehoge","fugaga"]};
+  }
+
   render() {
     const { title } = this.props
     const { comment } = this.props
-
-    let  items = ['test', 'hoge']
-    for (let i = 0; i < items.length; i++) {
-      console.log(i);
-      console.log(items[i]);
-    }
+    const { items } = this.state.items
 
     return (
       <div>
         <h1>{title}</h1>
         <h1>{comment}</h1>
+        <AppList items={this.state.items} />
       </div>
-    )
+    );
   }
 }
-
-render(
-  <App title="Hello React" comment="ほげホゲホゲ" />,
-  document.getElementById('container'))
+//
+// render(
+//   <App title="Hello React" comment="ほげホゲホゲ" />,
+//   document.getElementById('container'))
