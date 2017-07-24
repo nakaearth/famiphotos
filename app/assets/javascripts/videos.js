783ab92aa78895d2aa94fba7806450601585fb0d
nakaearth <naka5313@gmail.com>
2017-07-25 01:00:55 +0900

    動画のアップロードをV2 apiで出来そう。ただサイズがでかい

--- app/assets/javascripts/videos.js
+++ app/assets/javascripts/videos.js
@@ -1,6 +1,13 @@
-ZiggeoApi.Videos.index({}, {
-  success: function (args, videos) {
-    for (var i = 0; i < videos.length; ++i)
-      $("body").append("<ziggeo ziggeo-video='" + videos[i].token + "'></ziggeo>");
-    }
+//we wait for page load to be finished (you could use jQuery ready event as well, or any such alternative that you prefer)
+ZiggeoApi.Events.on("system_ready", function() {
+    //Lets get the ziggeo-recorder element reference
+    var element = document.getElementById('myRecorder');
+
+    //now lets get the actual Ziggeo embedding / object that we can use
+    var recorder = ZiggeoApi.V2.Recorder.findByElement(element);
+
+    // we can also create a global event to fire each time video was uploaded
+    recorder.on("verified", function () {
+        alert("The video with token '" + recorder.get('video') + "' has been submitted!");
+    });
 });
