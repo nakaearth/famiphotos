21fba1dc9c4bfcb7dd519cd57b77b96671a614eb
nakaearth <naka5313@gmail.com>
2017-08-01 14:52:47 +0900

    ログ出力先を修正

--- app/batches/setup_elasticsearch.rb
+++ app/batches/setup_elasticsearch.rb
@@ -9,7 +9,7 @@ require 'optparse'
 class SetupElasticsearch
   class << self
     def execute
-      logger = ActiveSupport::Logger.new("#{Rails.root}/log/setting_batch.log", 'daily')
+      logger = ActiveSupport::Logger.new("setting_batch.log", 'daily')
       force = args[:force] || false
 
       logger.info('index作成')
