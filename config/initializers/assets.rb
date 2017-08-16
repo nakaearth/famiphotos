1354d7f4a8ece75b6754721208eea8174693f6e1
nakaearth <naka5313@gmail.com>
2017-08-16 13:49:36 +0900

    videosの一覧取得の設定を変更

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,4 +9,4 @@ Rails.application.config.assets.version = '1.1'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-Rails.application.config.assets.precompile += %w( photos.js videos.js webpack/app.js )
+Rails.application.config.assets.precompile += %w( photos.js videos.js videos_list.js webpack/app.js )
