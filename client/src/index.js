8848343d6f59040bd9296c45761ff7b02e124643
nakaearth <naka5313@gmail.com>
2017-09-01 13:03:51 +0900

    設定を追加

--- client/src/index.js
+++ client/src/index.js
@@ -3,5 +3,5 @@ import { render } from 'react-dom'
 import { PhotoViewer, PhotoViewerList } from './photoViewer.js'
 
 render(
-  <PhotoViewer title="共有写真" url='/api/shared_albums' />,
+  <PhotoViewer title="共有写真" url='/api/informations' />,
   document.getElementById('container'))
