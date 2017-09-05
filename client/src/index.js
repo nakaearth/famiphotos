95c1499c26eedfa5d3533617d05742edb2dcf3c6
nakaearth <naka5313@gmail.com>
2017-09-05 16:26:52 +0900

    jsエラー対応

--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,8 @@ import { render } from 'react-dom'
 import { PhotoViewer, PhotoViewerList } from './photoViewer.js'
 
 var dir = location.href.split("/");
-// var id = dir[dir.length -1];
-
-console.log(dir)
-var search_url += '/api/shared_albumss/'
+var id = dir[dir.length -1];
+var search_url = '/api/shared_albumss/' + id;
 render(
-  <PhotoViewer title="共有写真" url=search_url pollInterval={2000} />,
+  <PhotoViewer title="共有写真" url={search_url} pollInterval={2000} />,
   document.getElementById('container'))
