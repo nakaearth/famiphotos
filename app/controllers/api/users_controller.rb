034361f3b4b3961118acd31401bcd767118b2810
nakaearth <naka5313@gmail.com>
2017-09-18 23:18:15 +0900

    apiを追加

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+module Api
+  class UsersController < Api::ApplicationController
+    include EncryptionConcern
+
+    def create
+      # TODO: 登録処理を追加
+    end
+  end
+end
