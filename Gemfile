30006f41c0ee44ff0ce507cea09bf91d02a784a9
nakaearth <naka5313@gmail.com>
2019-08-12 23:55:40 +0900

    rails5.2 update

--- Gemfile
+++ Gemfile
@@ -3,7 +3,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 ruby '2.6.3'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '5.1.6'
+gem 'rails', '5.2'
 gem 'rake', '~> 12.0'
 # Use mysql as the database for Active Record
 gem 'mysql2'
@@ -13,7 +13,7 @@ gem 'hiredis'
 gem 'redis', require: ['redis', 'redis/connection/hiredis']
 gem 'rack-cors'
 # Reduces boot times through caching; required in config/boot.rb
-gem 'bootsnap', '>= 1.1.0', require: false
+gem 'bootsnap', require: false
 
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 2.5'
--- config/application.rb
+++ config/application.rb
@@ -8,19 +8,13 @@ Bundler.require(*Rails.groups)
 
 module Famiphotos
   class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.0
+
     # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-    config.time_zone = 'Tokyo'
-    config.active_record.default_timezone = :local
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    config.i18n.default_locale = :ja
-    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb, yml}').to_s]
-
-    # add lib directory
-    config.autoload_paths << Rails.root.join('lib', 'constraints')
-
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
     config.generators do |g|
       g.orm :active_record, migration: false
       g.stylesheets false
