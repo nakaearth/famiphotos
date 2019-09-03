57b92d03f1210fb27f33730226d4931461e9419a
nakaearth <naka5313@gmail.com>
2019-09-04 00:33:31 +0900

    クラス追加

--- app/infrastructures/search/create_index_infrastructure.rb
+++ app/infrastructures/search/create_index_infrastructure.rb
@@ -3,6 +3,20 @@ module Search
   class CreateIndexInfrastructure < BaseSearchInfrastructure
     class << self
       def call(index_name)
+        Elasticsearch::Model.client = client
+        elasticsearch_client = Elasticsearch::Model.client
+        if client.indices.exists? index: index_name
+          client.indices.delete index: index_name
+        end
+
+        client.indices.create(
+          index: index_name,
+          include_type_name: true,
+          body: {
+            settings: Album.settings.to_hash,
+            mappings: Album.mappings.to_hash
+          }
+        )
       end
     end
   end
---
+++ app/usecases/search/create_index_usecase.rb
@@ -0,0 +1,7 @@
+module Search
+  class CreateIndexUsecase
+    def execute
+      # ここからinfrastureのcreate_index処理を呼ぶ
+    end
+  end
+end
