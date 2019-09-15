62ba0d7d12d8b7af5744ae3816efde441614e40d
nakaearth <naka5313@gmail.com>
2019-09-16 00:45:19 +0900

    テストケース追加

--- spec/queries/albums/album_search_query_spec.rb
+++ spec/queries/albums/album_search_query_spec.rb
@@ -4,21 +4,22 @@ require 'rails_helper'
 
 RSpec.describe Albums::AlbumSearchQuery do
   describe '.call' do
-    let(:user) { create(:user) }
-    let(:album) { create(:album, title: 'テスト犬album', user: user) }
-    let(:photo) { create(:photo, description: 'これは犬の写真です', album: album) }
-    let(:tag) { create(:tag, label_name: '犬', album: album) }
-
     before do
-      tag
       Search::ElasticsearchIndexGateway.create_index('albums')
-      Search::AlbumToElasticsearchInsertGateway.bulk('albums')
     end
 
-    context '該当するデータがある場合' do
-      let(:params) { { keyword: 'テスト犬' } }
+    context '1件だけ指定したユーザが作成したアルバムがある場合' do
+      let(:user) { create(:user) }
+      let(:album) { create(:album, title: 'テスト犬album', user: user) }
+      let(:photo) { create(:photo, description: 'これは犬の写真です',
+                           album: album, user: user) }
+      let(:tag) { create(:tag, label_name: '犬', album: album) }
+      let(:params) { { keyword: '犬' } }
 
       before do
+        tag
+        photo
+        Search::AlbumToElasticsearchInsertGateway.bulk('albums')
         @results = Albums::AlbumSearchQuery.call(keyword: params[:keyword], user_id: user.id)
       end
 
@@ -26,6 +27,46 @@ RSpec.describe Albums::AlbumSearchQuery do
         expect(@results.size).to eq 2
         expect(@results[:result_records].size).to eq 1
         expect(@results[:aggregations].empty?).to eq false
+      end
+
+      it 'keywordで指定したものが返ってくる' do
+        expect(@results[:result_records][0].id).to eq album.id
+        expect(@results[:result_records][0].title).to eq album.title
+      end
+    end
+
+    context '指定したユーザ以外が作成したアルバムもある場合' do
+      let(:user) { create(:user) }
+      let(:other_user) { create(:user) }
+      let(:album) { create(:album, title: 'テスト犬album', user: user) }
+      let(:photo) { create(:photo, description: 'これは犬の写真です',
+                           album: album, user: user) }
+      let(:tag) { create(:tag, label_name: '犬', album: album) }
+      let(:other_album) { create(:album, title: 'テスト犬album2', user: other_user) }
+      let(:other_photo) { create(:photo, description: 'これは最近買った犬の写真です',
+                                 album: other_album, user: other_user) }
+      let(:other_tag) { create(:tag, label_name: '犬', album: other_album) }
+
+      let(:params) { { keyword: '犬' } }
+
+      before do
+        photo
+        other_photo
+        tag
+        other_tag
+        Search::AlbumToElasticsearchInsertGateway.bulk('albums')
+        @results = Albums::AlbumSearchQuery.call(keyword: params[:keyword], user_id: user.id)
+      end
+
+      it '検索結果と検索結果の総数、アグリゲーションの結果が格納されたHashを返す' do
+        expect(@results.size).to eq 2
+        expect(@results[:result_records].size).to eq 1
+        expect(@results[:aggregations].empty?).to eq false
+      end
+
+      it 'keywordで指定したものが返ってくる' do
+        expect(@results[:result_records][0].id).to eq album.id
+        expect(@results[:result_records][0].title).to eq album.title
       end
     end
   end
