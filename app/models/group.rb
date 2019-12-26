ebc1144ad53999dfa56fed3d99c0e1cefd3d145a
nakaearth <naka5313@gmail.com>
2019-12-26 11:56:03 +0900

    モデルの追加

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 class Group < FamiphotoBase
-  belongs_to :user
+  belongs_to :owner, class_name: 'User'
 
   validates :name, presence: true, length: { maximum: 80 }
 end
